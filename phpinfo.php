1dee44c361a5b0bc0a7575ed7b4ebb76f16816e1
Kenta Shinohara <nocknocknock@gmail.com>
2015-09-10 19:29:33 +0900

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,11 @@
 <?php
 
+echo "<div class=\"balloon\"><br/>";
+echo "IP(VM) = 123.123.123.123<br/>";
+echo "IP(Container) = 10.1.2.3";
+echo "</div>"
+echo "<img src=\"logo.png\" width=\"400px\" />";
+
 phpinfo(INFO_VARIABLES);
 
 ?>
