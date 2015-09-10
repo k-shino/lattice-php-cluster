ef958e02b3160438189f78813dd30ce0c89be8d9
Kenta Shinohara <nocknocknock@gmail.com>
2015-09-10 20:08:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -54,12 +54,13 @@ function print_route()
 
 echo "<div class=\"balloon\"><br/>";
 echo "IP(VM) = ";
-echo $_ENV["SERVER_ADDR"];
+echo $_SERVER["SERVER_ADDR"];
 echo "<br/>";
 echo "IP(Container) =";
-echo $_SERVER["CF_INSTANCE_IP"];
+echo $_ENV["CF_INSTANCE_IP"];
 echo "</div>"
 echo "<img src=\"logo.png\" width=\"400px\" />";
+
 
 #echo "phpinfo(local container)";
 #phpinfo(INFO_VARIABLES);
