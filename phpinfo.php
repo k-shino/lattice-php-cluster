fa648b217408cd755335715f8e254275a8929b14
Kenta Shinohara <nocknocknock@gmail.com>
2015-09-10 20:05:35 +0900

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -8,16 +8,7 @@
 
 <?php
 
-echo "<div class=\"balloon\"><br/>";
-echo "IP(VM) = ";
-echo $_ENV["SERVER_ADDR"];
 echo "<br/>";
-echo "IP(Container) =";
-echo $_SERVER["CF_INSTANCE_IP"];
-echo "</div>"
-echo "<img src=\"logo.png\" width=\"400px\" />";
-
-phpinfo(INFO_VARIABLES);
 
 ?>
 	</div>
